c2a1c5489f26a5518be46447139036c0b4271b78
Bogost <bogumilostapiuk@gmail.com>
2019-05-09 15:46:58 +0200

    Utworzenie testu confirmShouldNotThrowExceptiomIfOrderIsSubmittedLessThan24HoursAgo()

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -20,10 +20,18 @@ public class OrderTest {
     }
 
     @Test(expected = OrderExpiredException.class)
-    public void confirmShouldThrowExceptiomIfOrderIsSubmittedMorThan24HoursAgo() {
+    public void confirmShouldThrowExceptiomIfOrderIsSubmittedMoreThan24HoursAgo() {
         TestableClock.timeTravel(LocalDateTime.parse("2018-01-01T00:00:00"));
         order.submit();
         TestableClock.timeTravel(LocalDateTime.parse("2018-01-03T00:00:00"));
+        order.confirm();
+    }
+
+    @Test(expected = Test.None.class)
+    public void confirmShouldNotThrowExceptiomIfOrderIsSubmittedLessThan24HoursAgo() {
+        TestableClock.timeTravel(LocalDateTime.parse("2018-01-01T00:00:00"));
+        order.submit();
+        TestableClock.timeTravel(LocalDateTime.parse("2018-01-01T23:59:59"));
         order.confirm();
     }
 
